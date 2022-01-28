b345854e802c2ea4bdc888687accfb189a129751
José Enrique <otipip90@gmail.com>
2022-01-27 20:02:52 -0500

    added logic into actions and service reducers

---
+++ src/Redux/actions/service.actions.js
@@ -0,0 +1,57 @@
+export const digitalServicesTypes = {
+  GET_DIGITAL_SERVICES: '[SERVICES] GET_DIGITAL_SERVICES',
+  REQUEST_DIGITAL_SERVICES: '[SERVICES] REQUEST_DIGITAL_SERVICES',
+  SUCCESS_DIGITAL_SERVICES: '[SERVICES] SUCCESS_DIGITAL_SERVICES',
+  FAILURE_DIGITAL_SERVICES: '[SERVICES] FAILURE_DIGITAL_SERVICES',
+};
+
+/**
+ * Digital Services
+ */
+
+export const digitalServices = () => ({
+  type: digitalServicesTypes.GET_DIGITAL_SERVICES,
+});
+
+export const digitalServicesRequest = () => ({
+  type: digitalServicesTypes.REQUEST_DIGITAL_SERVICES,
+});
+
+export const digitalServicesFailure = (error) => ({
+  type: digitalServicesTypes.FAILURE_DIGITAL_SERVICES,
+  payload: error,
+});
+
+export const digitalServicesSuccess = (response) => ({
+  type: digitalServicesTypes.SUCCESS_DIGITAL_SERVICES,
+  payload: response,
+});
+
+/**
+ *     INFORMATION:
+ * Get equipment
+ * @returns
+ */
+export const getDigitalServices = () => {
+  return (dispatch) => {
+    dispatch(digitalServicesRequest());
+
+    const onSuccess = (response) => {
+      dispatch(digitalServicesSuccess(response));
+      return response;
+    };
+    const onFailure = (error) => {
+      dispatch(digitalServicesFailure(error));
+      return error;
+    };
+
+    try {
+      digitalServices().then((data) => {
+        console.log(data);
+        onSuccess(data);
+      });
+    } catch (error) {
+      onFailure(error);
+    }
+  };
+};
---
+++ src/Redux/reducers/service.reducers.js
@@ -0,0 +1,31 @@
+import { digitalServicesTypes } from '../actions/service.actions';
+
+const initialState = {
+  services: [],
+  isFetching: false,
+};
+
+const servicesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case digitalServicesTypes.GET_DIGITAL_SERVICES:
+      return {
+        ...state,
+      };
+    case digitalServicesTypes.SUCCESS_DIGITAL_SERVICES:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+      };
+    case digitalServicesTypes.REQUEST_DIGITAL_SERVICES:
+      return {
+        ...state,
+        isFetching: true,
+      };
+
+    default:
+      return { ...state };
+  }
+};
+
+export default servicesReducer;
